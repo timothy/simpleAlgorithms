16f4f703e81035ffcb90aca973186cf1086910a1
tabradford <tabradford@liberty.edu>
2016-09-24 11:13:21 -0400

    Added reverseString() and factorialize()

--- simpleAlgorithms/simpleAlgorithms.cpp
+++ simpleAlgorithms/simpleAlgorithms.cpp
@@ -2,10 +2,42 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+string reverseString(string str)
+{
+	string reverse = "";
+
+	for (int i = 0; i<str.length(); i++)
+	{
+		reverse += str[(str.length() -1) - i];
+	}
+	return reverse;
+}
+
+auto factorialize(int num) {
+	if (num == 1) {
+		return 1;
+	}
+	auto factorial = 1;
+	for (auto i = 2; i <= num; i++) {
+		factorial = factorial * i;
+	}
+
+	return factorial;
+}
 
 int main()
 {
-    return 0;
-}
+	string tim = "Timothy";
+	int num = 10;
 
+	cout << reverseString(tim) << endl;
+
+	cout << factorialize(num) << endl;
+
+	return 0;
+}
